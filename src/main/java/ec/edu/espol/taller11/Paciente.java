b686dd80883a7be2120eb3184c75e4a64785ccec
Bryan Zhang <140031745+bgzhangg@users.noreply.github.com>
2024-08-07 18:21:50 -0500

    cambios en paciente
    Agregado constante de edad adulto mayor y encapsulado campo "historial medico"

--- src/main/java/ec/edu/espol/taller11/Paciente.java
+++ src/main/java/ec/edu/espol/taller11/Paciente.java
@@ -1,7 +1,8 @@
 package ec.edu.espol.taller11;
 
 public class Paciente extends Persona {
-    public HistorialMedico historialMedico;
+    private static int EdadAdultoMayor = 65;
+    private HistorialMedico historialMedico;
     
     public Paciente(String nombre, int edad, String genero, String direccion, String telefono, String correoElectronico) {
         super(nombre, edad, genero, direccion, telefono, correoElectronico);
@@ -9,6 +10,15 @@ public class Paciente extends Persona {
     }
 
     public boolean esAdultoMayor(){
-        return super.getEdad()>=65;
+        return super.getEdad()>= EdadAdultoMayor;
     }
+
+    public HistorialMedico getHistorialMedico() {
+        return historialMedico;
+    }
+
+    public void setHistorialMedico(HistorialMedico historialMedico) {
+        this.historialMedico = historialMedico;
+    }
+    
 }
